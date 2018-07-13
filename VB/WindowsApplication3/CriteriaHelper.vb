e6be11a928bf4eb88b7865082efceb9101b2d002
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-07-13 13:50:18 +0300

    update to 15.1 + fix BC2981

--- VB/WindowsApplication3/CriteriaHelper.vb
+++ VB/WindowsApplication3/CriteriaHelper.vb
@@ -25,120 +25,120 @@ Namespace DXSample
 	End Class
 
 	Public Class RemoveCriteriaVisitor
-		Implements IClientCriteriaVisitor
-		Private fieldToRemove As String = String.Empty
-		Public Sub New()
-		End Sub
+        Implements IClientCriteriaVisitor(Of CriteriaOperator)
+        Private fieldToRemove As String = String.Empty
+        Public Sub New()
+        End Sub
 
-		Public Function RemoveCriteriaByFieldName(ByVal fieldToRemove As String, ByVal op As CriteriaOperator) As CriteriaOperator
-			If String.IsNullOrEmpty(fieldToRemove) Then
-				Return Nothing
-			End If
-			Me.fieldToRemove = fieldToRemove
-			Return TryCast(op.Accept(Me), CriteriaOperator)
-		End Function
+        Public Function RemoveCriteriaByFieldName(ByVal fieldToRemove As String, ByVal op As CriteriaOperator) As CriteriaOperator
+            If String.IsNullOrEmpty(fieldToRemove) Then
+                Return Nothing
+            End If
+            Me.fieldToRemove = fieldToRemove
+            Return TryCast(op.Accept(Me), CriteriaOperator)
+        End Function
 
-		#Region "IClientCriteriaVisitor Members"
+#Region "IClientCriteriaVisitor Members"
 
-		Public Function Visit(ByVal theOperand As JoinOperand) As Object Implements IClientCriteriaVisitor.Visit
-			Dim condition As CriteriaOperator = TryCast(theOperand.Condition.Accept(Me), CriteriaOperator)
-			Dim expression As CriteriaOperator = TryCast(theOperand.AggregatedExpression.Accept(Me), CriteriaOperator)
-			If Object.ReferenceEquals(condition, Nothing) OrElse Object.ReferenceEquals(expression, Nothing) Then
-				Return Nothing
-			End If
-			Return New JoinOperand(theOperand.JoinTypeName, condition, theOperand.AggregateType, expression)
-		End Function
+        Public Function Visit(ByVal theOperand As JoinOperand) As CriteriaOperator Implements IClientCriteriaVisitor(Of CriteriaOperator).Visit
+            Dim condition As CriteriaOperator = TryCast(theOperand.Condition.Accept(Me), CriteriaOperator)
+            Dim expression As CriteriaOperator = TryCast(theOperand.AggregatedExpression.Accept(Me), CriteriaOperator)
+            If Object.ReferenceEquals(condition, Nothing) OrElse Object.ReferenceEquals(expression, Nothing) Then
+                Return Nothing
+            End If
+            Return New JoinOperand(theOperand.JoinTypeName, condition, theOperand.AggregateType, expression)
+        End Function
 
-		Public Function Visit(ByVal theOperand As OperandProperty) As Object Implements IClientCriteriaVisitor.Visit
-			If theOperand.PropertyName = fieldToRemove Then
-				Return Nothing
-			End If
-			Return theOperand
-		End Function
+        Public Function Visit(ByVal theOperand As OperandProperty)  As CriteriaOperator Implements IClientCriteriaVisitor(Of CriteriaOperator).Visit
+            If theOperand.PropertyName = fieldToRemove Then
+                Return Nothing
+            End If
+            Return theOperand
+        End Function
 
-		Public Function Visit(ByVal theOperand As AggregateOperand) As Object Implements IClientCriteriaVisitor.Visit
-			Dim operand As OperandProperty = TryCast(theOperand.CollectionProperty.Accept(Me), OperandProperty)
-			Dim condition As CriteriaOperator = TryCast(theOperand.Condition.Accept(Me), CriteriaOperator)
-			Dim expression As CriteriaOperator = TryCast(theOperand.AggregatedExpression.Accept(Me), CriteriaOperator)
-			If Object.ReferenceEquals(condition, Nothing) OrElse Object.ReferenceEquals(expression, Nothing) OrElse Object.ReferenceEquals(operand, Nothing) Then
-				Return Nothing
-			End If
-			Return New AggregateOperand(operand, expression, theOperand.AggregateType, condition)
-		End Function
-		#End Region
+        Public Function Visit(ByVal theOperand As AggregateOperand)  As CriteriaOperator Implements IClientCriteriaVisitor(Of CriteriaOperator).Visit
+            Dim operand As OperandProperty = TryCast(theOperand.CollectionProperty.Accept(Me), OperandProperty)
+            Dim condition As CriteriaOperator = TryCast(theOperand.Condition.Accept(Me), CriteriaOperator)
+            Dim expression As CriteriaOperator = TryCast(theOperand.AggregatedExpression.Accept(Me), CriteriaOperator)
+            If Object.ReferenceEquals(condition, Nothing) OrElse Object.ReferenceEquals(expression, Nothing) OrElse Object.ReferenceEquals(operand, Nothing) Then
+                Return Nothing
+            End If
+            Return New AggregateOperand(operand, expression, theOperand.AggregateType, condition)
+        End Function
+#End Region
 
-		#Region "ICriteriaVisitor Members"
+#Region "ICriteriaVisitor Members"
 
-		Public Function Visit(ByVal theOperator As FunctionOperator) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Dim operators As New List(Of CriteriaOperator)()
-			For Each op As CriteriaOperator In theOperator.Operands
-				Dim temp As CriteriaOperator = TryCast(op.Accept(Me), CriteriaOperator)
-				If Object.ReferenceEquals(temp, Nothing) Then
-					Return Nothing
-				End If
-				operators.Add(temp)
-			Next op
-			Return New FunctionOperator(theOperator.OperatorType, operators)
-		End Function
+        Public Function Visit(ByVal theOperator As FunctionOperator) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Dim operators As New List(Of CriteriaOperator)()
+            For Each op As CriteriaOperator In theOperator.Operands
+                Dim temp As CriteriaOperator = TryCast(op.Accept(Me), CriteriaOperator)
+                If Object.ReferenceEquals(temp, Nothing) Then
+                    Return Nothing
+                End If
+                operators.Add(temp)
+            Next op
+            Return New FunctionOperator(theOperator.OperatorType, operators)
+        End Function
 
-		Public Function Visit(ByVal theOperand As OperandValue) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Return theOperand
-		End Function
+        Public Function Visit(ByVal theOperand As OperandValue) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Return theOperand
+        End Function
 
-		Public Function Visit(ByVal theOperator As GroupOperator) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Dim operators As New List(Of CriteriaOperator)()
-			For Each op As CriteriaOperator In theOperator.Operands
-				Dim temp As CriteriaOperator = TryCast(op.Accept(Me), CriteriaOperator)
-				If Object.ReferenceEquals(temp, Nothing) Then
-					Continue For
-				End If
-				operators.Add(temp)
-			Next op
-			Return New GroupOperator(theOperator.OperatorType, operators)
-		End Function
+        Public Function Visit(ByVal theOperator As GroupOperator) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Dim operators As New List(Of CriteriaOperator)()
+            For Each op As CriteriaOperator In theOperator.Operands
+                Dim temp As CriteriaOperator = TryCast(op.Accept(Me), CriteriaOperator)
+                If Object.ReferenceEquals(temp, Nothing) Then
+                    Continue For
+                End If
+                operators.Add(temp)
+            Next op
+            Return New GroupOperator(theOperator.OperatorType, operators)
+        End Function
 
-		Public Function Visit(ByVal theOperator As InOperator) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Dim leftOperand As CriteriaOperator = TryCast(theOperator.LeftOperand.Accept(Me), CriteriaOperator)
-			Dim operators As New List(Of CriteriaOperator)()
-			For Each op As CriteriaOperator In theOperator.Operands
-				Dim temp As CriteriaOperator = TryCast(op.Accept(Me), CriteriaOperator)
-				If Object.ReferenceEquals(temp, Nothing) Then
-					Continue For
-				End If
-				operators.Add(temp)
-			Next op
-			If Object.ReferenceEquals(leftOperand, Nothing) Then
-				Return Nothing
-			End If
-			Return New InOperator(leftOperand, operators)
-		End Function
+        Public Function Visit(ByVal theOperator As InOperator) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Dim leftOperand As CriteriaOperator = TryCast(theOperator.LeftOperand.Accept(Me), CriteriaOperator)
+            Dim operators As New List(Of CriteriaOperator)()
+            For Each op As CriteriaOperator In theOperator.Operands
+                Dim temp As CriteriaOperator = TryCast(op.Accept(Me), CriteriaOperator)
+                If Object.ReferenceEquals(temp, Nothing) Then
+                    Continue For
+                End If
+                operators.Add(temp)
+            Next op
+            If Object.ReferenceEquals(leftOperand, Nothing) Then
+                Return Nothing
+            End If
+            Return New InOperator(leftOperand, operators)
+        End Function
 
-		Public Function Visit(ByVal theOperator As UnaryOperator) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Dim operand As CriteriaOperator = TryCast(theOperator.Operand.Accept(Me), CriteriaOperator)
-			If Object.ReferenceEquals(operand, Nothing) Then
-				Return Nothing
-			End If
-			Return New UnaryOperator(theOperator.OperatorType, operand)
-		End Function
+        Public Function Visit(ByVal theOperator As UnaryOperator) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Dim operand As CriteriaOperator = TryCast(theOperator.Operand.Accept(Me), CriteriaOperator)
+            If Object.ReferenceEquals(operand, Nothing) Then
+                Return Nothing
+            End If
+            Return New UnaryOperator(theOperator.OperatorType, operand)
+        End Function
 
-		Public Function Visit(ByVal theOperator As BinaryOperator) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Dim leftOperand As CriteriaOperator = TryCast(theOperator.LeftOperand.Accept(Me), CriteriaOperator)
-			Dim rightOperand As CriteriaOperator = TryCast(theOperator.RightOperand.Accept(Me), CriteriaOperator)
-			If Object.ReferenceEquals(leftOperand, Nothing) OrElse Object.ReferenceEquals(rightOperand, Nothing) Then
-				Return Nothing
-			End If
-			Return New BinaryOperator(leftOperand, rightOperand, theOperator.OperatorType)
-		End Function
+        Public Function Visit(ByVal theOperator As BinaryOperator) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Dim leftOperand As CriteriaOperator = TryCast(theOperator.LeftOperand.Accept(Me), CriteriaOperator)
+            Dim rightOperand As CriteriaOperator = TryCast(theOperator.RightOperand.Accept(Me), CriteriaOperator)
+            If Object.ReferenceEquals(leftOperand, Nothing) OrElse Object.ReferenceEquals(rightOperand, Nothing) Then
+                Return Nothing
+            End If
+            Return New BinaryOperator(leftOperand, rightOperand, theOperator.OperatorType)
+        End Function
 
-		Public Function Visit(ByVal theOperator As BetweenOperator) As Object Implements DevExpress.Data.Filtering.ICriteriaVisitor.Visit
-			Dim test As CriteriaOperator = TryCast(theOperator.TestExpression.Accept(Me), CriteriaOperator)
-			Dim begin As CriteriaOperator = TryCast(theOperator.BeginExpression.Accept(Me), CriteriaOperator)
-			Dim [end] As CriteriaOperator = TryCast(theOperator.EndExpression.Accept(Me), CriteriaOperator)
-			If Object.ReferenceEquals(test, Nothing) OrElse Object.ReferenceEquals(begin, Nothing) OrElse Object.ReferenceEquals([end], Nothing) Then
-				Return Nothing
-			End If
-			Return New BetweenOperator(test, begin, [end])
-		End Function
-		#End Region
-	End Class
+        Public Function Visit(ByVal theOperator As BetweenOperator) As CriteriaOperator Implements ICriteriaVisitor(Of CriteriaOperator).Visit
+            Dim test As CriteriaOperator = TryCast(theOperator.TestExpression.Accept(Me), CriteriaOperator)
+            Dim begin As CriteriaOperator = TryCast(theOperator.BeginExpression.Accept(Me), CriteriaOperator)
+            Dim [end] As CriteriaOperator = TryCast(theOperator.EndExpression.Accept(Me), CriteriaOperator)
+            If Object.ReferenceEquals(test, Nothing) OrElse Object.ReferenceEquals(begin, Nothing) OrElse Object.ReferenceEquals([end], Nothing) Then
+                Return Nothing
+            End If
+            Return New BetweenOperator(test, begin, [end])
+        End Function
+#End Region
+    End Class
 End Namespace
