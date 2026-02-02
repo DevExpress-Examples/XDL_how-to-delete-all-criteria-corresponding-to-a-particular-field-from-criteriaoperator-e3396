7d9fde9302a690cd91fa0b41cd74131cb90c8330
DevExpressExampleBot <fakeEmail>
2026-02-02 21:57:42 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication3/Program.vb
+++ VB/WindowsApplication3/Program.vb
@@ -1,23 +1,21 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.Windows.Forms
 Imports DevExpress.Skins
 
 Namespace DXSample
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			DevExpress.UserSkins.BonusSkins.Register()
-			SkinManager.EnableFormSkins()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Main())
-		End Sub
-	End Class
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            DevExpress.UserSkins.BonusSkins.Register()
+            Call SkinManager.EnableFormSkins()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New Main())
+        End Sub
+    End Module
 End Namespace
